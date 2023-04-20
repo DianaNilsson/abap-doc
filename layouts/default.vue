60154f795a365320c40097b2c01942403ab16b68
Diana <diana.sofie.nilsson@gmail.com>
2023-04-20 15:00:47 +0200

    Adding pages and routes

---
+++ layouts/default.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <header>
+      <nav>
+        <ul>
+          <li><NuxtLink to="/">Home</NuxtLink></li>
+          <li><NuxtLink to="/introduction">Introduction</NuxtLink></li>
+        </ul>
+      </nav>
+    </header>
+    <div><slot /></div>
+  </div>
+</template>
+
+<style scoped>
+  .router-link-exact-active {
+    color: darkseagreen;
+  }
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h2>ABAP Programming Guide</h2>
+    <h3>
+      A Comprehensive Guide to Learn ABAP Programming from Scratch, Including
+      Data Types, Control Structures, and Advanced Concepts
+    </h3>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped></style>
